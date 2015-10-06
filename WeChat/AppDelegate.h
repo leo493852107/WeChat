428b69d876c9605a8eda8bde97c122907d02ef0c
leo <leo@leodeMacBook-Pro.local>
2015-10-06 09:21:27 +0800

    add XMPP to project

---
+++ WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by leo on 15/10/6.
+//  Copyright © 2015年 leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by leo on 15/10/6.
+//  Copyright © 2015年 leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
